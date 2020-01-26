416bcf94f0debe203a1cfbc831a591ed8e116ec5
Androz2091 <androz2091@gmail.com>
2020-01-26 20:37:00 +0100

    :tada: 1.0.0

---
+++ index.js
@@ -0,0 +1,81 @@
+console.log("[!] Starting...");
+
+// Récupération des données de la config
+const {
+    INSTA_USERNAME, // Pseudo du compte à observer
+    BOT_TOKEN, // Token du bot discord
+    POSTS_CHANNEL, // Channel dans lequel il faut poster les nouveaux posts
+    MESSAGE, // Message formaté pour les nouveaux posts
+
+    INSTA_INTERVAL, // L'interval en ms pour check les nouveaux posts
+    MESSAGE_UPDATE_INTERVAL, // L'interval de temps pour mettre à jour les likes
+    FORCE_PUSH_START // Si le message pour le dernier post doit être envoyé au démarrage
+} = require("./config.json");
+
+// Initialisation du client Instagram
+const Insta = require("@kaki87/ig-scraper");
+const InstaClient = new Insta();
+
+// Initialisation du client Discord
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+// Connexion à Discord
+client.login(BOT_TOKEN);
+
+client.on("ready", async () => {
+    console.log("[!] Ready. Logged as " + client.user.tag + ".");
+
+    // Récupération du dernier post
+    let lastPost = null;
+    let user = await InstaClient.getProfile(INSTA_USERNAME);
+    lastPost = user.lastPosts[0].shortcode;
+    console.log("[!] Last post found: " + lastPost);
+
+    // Salon des posts
+    let postsChannel = client.channels.get(POSTS_CHANNEL);
+    console.log("[!] Posts will be sent in #" + postsChannel.name);
+
+    console.log("[!] Starting like updates.");
+    updateLikes(postsChannel, InstaClient);
+    // Toutes les heures, mise à jour du compteur de likes sur chaque message
+    setInterval(
+        updateLikes,
+        MESSAGE_UPDATE_INTERVAL,
+        postsChannel,
+        InstaClient
+    );
+
+    // Envoi d'un post
+    const sendPost = async shortcode => {
+        let post = await InstaClient.getPost(shortcode);
+        postsChannel.send(
+            MESSAGE
+                // Lien du post
+                .replace("{{link}}", post.link)
+                // Message du post
+                .replace("{{caption}}", post.caption)
+                // Nombre de likes
+                .replace("{{likeCount}}", post.likes)
+        );
+    };
+
+    // Si aucun message n'est encore envoyé, envoi du dernier post
+    let messages = await postsChannel.fetchMessages();
+    if (
+        FORCE_PUSH_START ||
+        messages.size === 0 ||
+        process.argv.includes("--push-last")
+    )
+        sendPost(lastPost);
+
+    // Pour les prochains posts
+    InstaClient.subscribeUserPosts(INSTA_USERNAME, INSTA_INTERVAL).subscribe({
+        next: shortcode => {
+            sendPost(shortcode);
+        },
+        error: err => {
+            console.error(err);
+        }
+    });
+});
---
+++ updateLikes.js
@@ -0,0 +1,23 @@
+module.exports = async (channel, InstaClient) => {
+    // Fetch les 10 derniers messages du salon
+    let messages = await channel.fetchMessages({ limit: 10 });
+    messages
+        // Garde seulement les messages du bot
+        .filter(m => m.author.id === channel.client.user.id)
+        // Les trie par date
+        .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
+        .forEach(async message => {
+            // Parse le message pour obtenir le shortcode dans le lien
+            let shortcode = message.match(
+                /(https:\/\/www\.instagram\.com\/p\/)([\w]{11})/
+            )[1];
+            // Récupère le post mis à jour
+            let post = await InstaClient.getPost(shortcode);
+            // Mets à jour le nombre de likes
+            let oldLikeCount = message.split(": ").pop();
+            let updatedMessage = message.replace(oldLikeCount, post.likes);
+            // Edite le message
+            message.edit(updatedMessage);
+            console.log("[!] Message #" + message.id + " updated.");
+        });
+};
