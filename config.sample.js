67315714adb2331943f8b1531b02b4a7e09e7836
Androz2091 <androz2091@gmail.com>
2020-02-16 23:10:35 +0100

    Create config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+    token: "",
+    prefix: "!",
+    color: "#FF0000",
+    footer: "Cyanide and Happiness Bot",
+    emotes: {
+        error: ":x:",
+        success: ":white_check_mark:"
+    },
+    postChannel: "",
+    postFormat: ":fire: **NOUVEAU POST**\n\n> {link}",
+    status: "poster ",
+    insta: "cyanidehappinessvf2",
+    developers: [],
+    owners: []
+};
