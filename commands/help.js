cf858c3b5379cd664287fdbfc3d39228b905bc63
Androz2091 <androz2091@gmail.com>
2020-03-17 19:59:42 +0100

    fix: use client config instead of prefix

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ class Help extends Command {
     }
 
     async run(message, args, data) {
-        message.channel.send(":crown: **Fondateurs**\r\n\r\n`!upload`: Transfert de nouvelles publications vers le serveur.\r\n`!post`: Poste de nouvelles publications.\r\n\r\n:man_detective: **D\u00E9veloppeur**\r\n\r\n`!eval`: Ex\u00E9cute du code javascript.\r\n\r\n:baby_chick: **Utilisateurs**\r\n\r\n`!random`: Affiche une publication al\u00E9atoire.\r\n`!insta`: Affiche des informations sur le compte Instagram de Cyanide and Happiness VF.");
+        message.channel.send(":crown: **Fondateurs**\r\n\r\n`"+this.client.config.prefix+"upload`: Transfert de nouvelles publications vers le serveur.\r\n`"+this.client.config.prefix+"post`: Poste de nouvelles publications.\r\n\r\n:man_detective: **D\u00E9veloppeur**\r\n\r\n`"+this.client.config.prefix+"eval`: Ex\u00E9cute du code javascript.\r\n\r\n:baby_chick: **Utilisateurs**\r\n\r\n`"+this.client.config.prefix+"random`: Affiche une publication al\u00E9atoire.\r\n`"+this.client.config.prefix+"insta`: Affiche des informations sur le compte Instagram de Cyanide and Happiness VF.");
     }
 }
 
